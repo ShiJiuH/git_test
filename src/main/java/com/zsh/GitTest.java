2f01bc63bfea20360cd67ae6d8565e0d39d86eb0
Hao <Hao@974.com>
2022-04-26 16:32:17 +0800

    first commmit

---
+++ src/main/java/com/zsh/GitTest.java
@@ -0,0 +1,11 @@
+package com.zsh;
+
+/**
+ * @author zhangshihao
+ * @create 2022-04-26 16:26
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
